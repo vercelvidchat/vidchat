15fa3e5107d7a0cee7cb69640b333f9903004ae8
vercelvidchat <oliayt97+vidchat@gmail.com>
2024-12-11 19:23:37 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,29 +2,35 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 let waitingUsers = [];
+let userCount = 0; // This will track the number of users connected
 
 wss.on('connection', ws => {
-  console.log('A user connected');
-  
+  // Assign a unique identifier (ID) to the user based on the number of users
+  const userId = ++userCount;
+  console.log(`User ${userId} connected`);
+
+  // Send the unique userId to the client when they connect
+  ws.send(JSON.stringify({ type: 'userId', userId: userId }));
+
   // Add the user to the waiting list
-  waitingUsers.push(ws);
-  
-  // Match with a random user when two users are available
+  waitingUsers.push({ ws, userId });
+
+  // When two users are connected, match them and establish a connection
   if (waitingUsers.length >= 2) {
     const user1 = waitingUsers.pop();
     const user2 = waitingUsers.pop();
 
-    user1.send('Matched with a random person!');
-    user2.send('Matched with a random person!');
+    // Notify both users that they are matched
+    user1.ws.send(JSON.stringify({ type: 'matched', peerId: user2.userId }));
+    user2.ws.send(JSON.stringify({ type: 'matched', peerId: user1.userId }));
 
-    // Send the signaling data for WebRTC (simplified)
-    user1.send(JSON.stringify({ type: 'startChat', peerId: user2._socket.remoteAddress }));
-    user2.send(JSON.stringify({ type: 'startChat', peerId: user1._socket.remoteAddress }));
+    // Optionally, add logic to initiate the WebRTC signaling process here (simplified)
+    console.log(`Users ${user1.userId} and ${user2.userId} matched!`);
   }
 
   ws.on('message', message => {
-    console.log('Received message: ', message);
-    // Broadcast the message to all connected clients
+    console.log(`Received message from user ${userId}: `, message);
+    // Broadcast the message to the matched user(s)
     wss.clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
@@ -33,9 +39,9 @@ wss.on('connection', ws => {
   });
 
   ws.on('close', () => {
-    console.log('A user disconnected');
+    console.log(`User ${userId} disconnected`);
     // Remove user from the waiting list when they disconnect
-    waitingUsers = waitingUsers.filter(client => client !== ws);
+    waitingUsers = waitingUsers.filter(client => client.ws !== ws);
   });
 });
 
